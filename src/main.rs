42ff48807857b2661d3484a834e58aa6996789cf
hemanthbhusha <hemanthbhushan6@gmail.com>
2024-04-12 19:00:59 +0530

    added

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,53 @@
-// Define a Node struct representing each element in the linked list
+#[derive(Debug)]
 struct Node<T> {
-    data: T,
-    next: Option<Box<Node<T>>>,
+    data : T ,
+    next : Option <Box<Node<T>>>
 }
 
-// Define a LinkedList struct to hold the head of the list
-struct LinkedList<T> {
-    head: Option<Box<Node<T>>>,
+#[derive(Debug)]
+struct Linkedlist<T> {
+    head :  Option<Box<Node<T>>>,
 }
 
-impl<T : std::fmt::Display > LinkedList<T> {
-    // Constructor to create an empty linked list
-    fn new() -> Self {
-        LinkedList { head: None }
-    }
-
-    // Method to push a new element to the front of the linked list
-    fn push(&mut self, data: T) {
-        let new_node = Node {
-            data: data,
-            next: self.head.take(),
-        };
-        self.head = Some(Box::new(new_node));
-    }
-
-    // Method to pop the first element from the linked list
-    fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|mut node| {
-            self.head = node.next.take();
-            node.data
-        })
-    }
-
-    // Method to check if the linked list is empty
-    fn is_empty(&self) -> bool {
-        self.head.is_none()
-    }
-
-    // Method to print the linked list
-    fn print(&self) {
-        let mut current = &self.head;
-        print!("List: ");
-        while let Some(node) = current {
-            print!("{} -> ", node.data);
-            current = &node.next;
+impl<T : std::fmt::Debug +  std::fmt::Display> Linkedlist<T> {
+    fn new() -> Linkedlist<T>{
+        Linkedlist{
+            head : None
         }
-        println!("None");
     }
+
+    fn push(&mut self , data : T) {
+        let next = self.head.take();
+        let node = Node{
+            data  ,
+            next
+        }; 
+        self.head = Some(Box::new(node))
+    }
+
+    fn pop(&mut self) { 
+    //     let current_node = self.head.take().unwrap();
+    //    self.head = current_node.next 
+    let data = self.head.take().map(| node| {
+         self.head = node.next; 
+        node.data
+    });
 }
+}
+    
 
-fn main() {
-    // Create a new linked list
-    let mut list: LinkedList<i32> = LinkedList::new();
+fn  main() {
 
-    // Push some elements to the linked list
-    list.push(1);
-    list.push(2);
-    list.push(3);
+    let mut linkedlist : Linkedlist<i32>= Linkedlist::new();
+     println!("linkedlist created : {:?}" , linkedlist);
 
-    // Print the linked list
-    list.print();
+     linkedlist.push(32);
+    //  linkedlist.push(33332);
+    //  linkedlist.push(344546542);
+     
+     println!("linkedlist created : {:?}" , linkedlist);
+    linkedlist.pop();
+    println!("linkedlist created : {:?}" , linkedlist);
 
-    // Pop an element from the linked list
-    if let Some(data) = list.pop() {
-        println!("Popped element: {}", data);
-    }
 
-    // Print the linked list again
-    list.print();
 }
